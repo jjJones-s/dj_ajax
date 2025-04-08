fdb3d886cbca3c6be66f2d8c86d754f60e77a45b
Joshua Jones <jrj2004@hotmail.ca>
2025-04-08 00:59:49 -0400

    added fucntionality to get post obj with ajax calls and display in the dom

--- src/profiles/static/posts/main.js
+++ src/profiles/static/posts/main.js
@@ -1,19 +1,44 @@
 console.log("Hello world");
 
 const helloWorldBox = document.getElementById('hello-world');
+const postsBox = document.getElementById('posts-box');
 
 $.ajax({
 
   type: 'GET',
   url: '/hello-world/',
   success: function (response) {
-    console.log('success', response);
+    console.log('success:', response);
     helloWorldBox.textContent = response.text;
   },
 
   error: function (error) {
     console.error('Error:', error);
   }
+
+
+})
+
+$.ajax({
+  type: 'GET',
+  url: '/data/',
+  success: function (response) {
+    console.log('success:', response);
+    const data = response.data;
+
+    console.log('Data:', data);
+
+    data.forEach(el => {
+      postsBox.innerHTML += `
+        ${el.title} - <b>${el.body}</b> <br>
+        `
+  })
+},
+
+  error: function (error) {
+    console.error('Error:', error);
+  }
+
 
 
 })
