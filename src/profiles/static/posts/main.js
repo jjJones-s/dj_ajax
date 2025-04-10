f13ac1ce2f674114de898aad7d8553f6cdc84c37
Joshua Jones <jrj2004@hotmail.ca>
2025-04-09 23:29:39 -0400

    Added the spinner, Created posts card view, added laoding more posts witha  button click

--- src/profiles/static/posts/main.js
+++ src/profiles/static/posts/main.js
@@ -2,43 +2,78 @@ console.log("Hello world");
 
 const helloWorldBox = document.getElementById('hello-world');
 const postsBox = document.getElementById('posts-box');
+const spinnerBox = document.getElementById('spinner-box');
+const loadBtn = document.getElementById('load-more');
+const endBox = document.getElementById('end-box');
 
-$.ajax({
+let visible = 3;
 
-  type: 'GET',
-  url: '/hello-world/',
-  success: function (response) {
-    console.log('success:', response);
-    helloWorldBox.textContent = response.text;
+const getData = () =>{
+  $.ajax({
+    type: 'GET',
+    url: `/data/${visible}/`,
+    success: function (response) {
+      console.log('success:', response);
+      const data = response.data;
+  
+      setTimeout(() => {
+  
+        spinnerBox.classList.add('not-visible');
+  
+        console.log('Data:', data);
+    
+        data.forEach(el => {
+          postsBox.innerHTML += `
+  
+            <div class="card mb-2">
+          <div class="card-body">
+            <h5 class="card-title">${el.title}</h5>
+            <p class="card-text">${el.body}</p>
+          </div>
+          <div class="card-footer">
+            <div class="row">
+              <div class="col-1">
+                <a href="#" class="btn btn-primary">Details</a>
+              </div>
+              <div class="col-1">
+                <a href="#" class="btn btn-primary">Like</a>
+              </div>
+            </div>
+          </div>
+        </div>
+            `
+      })
+        
+      }, 100)
+
+      console.log(response.size)
+      if(response.size ===0){
+        endBox.textContent = "No posts added yet...";
+      }
+      else if (response.size <= visible) {
+        loadBtn.classList.add('not-visible');
+        endBox.textContent = "No more posts to load...";
+      
+      }
+  
   },
-
-  error: function (error) {
-    console.error('Error:', error);
-  }
-
-
-})
-
-$.ajax({
-  type: 'GET',
-  url: '/data/',
-  success: function (response) {
-    console.log('success:', response);
-    const data = response.data;
-
-    console.log('Data:', data);
-
-    data.forEach(el => {
-      postsBox.innerHTML += `
-        ${el.title} - <b>${el.body}</b> <br>
-        `
+  
+    error: function (error) {
+      console.error('Error:', error);
+    }
+  
+  
+  
   })
-},
-
-  error: function (error) {
-    console.error('Error:', error);
-  }
+}
 
 
+loadBtn.addEventListener('click', () => {
+
+  spinnerBox.classList.remove('not-visible');
+  visible += 3;
+  getData();
 
 })
+
+getData();
